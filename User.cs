456dcb072b299a06e92c537253355a241c4efa3c
Sravani Kunapuli <ckt3812@autuni.ac.nz>
2026-04-09 15:04:24 +1200

    Initial commit

---
+++ User.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace MainfreightProject
+{
+    public class User
+    {
+        private string userID;
+        private string name;
+        private string email;
+
+        public User(string userID, string name, string email)
+        {
+        }
+
+        public string ViewProfile()
+        {
+            return "";
+        }
+    }
+}
